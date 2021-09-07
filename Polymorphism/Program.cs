5f5c0cb833f4b2e022a27df656047d567cafa980
Herman Benao <hermben@gmail.com>
2021-09-06 19:53:56 -0700

    fixes issues with failed unitTest

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -5,13 +5,13 @@ namespace Polymorphism
 {
     public class Employee
     {
-        public virtual void CalculateWeeklySalary(int weeklyHours, int wage)
+        public virtual string CalculateWeeklySalary(int weeklyHours, int wage)
         {
             var salary = 40 * wage;
-            string result = string.Format("\nThis ANGRY EMPLOYEE worked {0} hrs. " + 
-                              "Paid for 40 hrs at $ {1}" +
-                              "/hr = ${2} \n", weeklyHours, wage, salary);
-            Console.WriteLine("\n" + result + "\n");
+            string result = String.Format("This ANGRY EMPLOYEE worked {0} hrs. " +
+                            "Paid for 40 hrs at $ {1}" +
+                            "/hr = ${2}", weeklyHours, wage, salary);
+            Console.WriteLine("\n" + result + " \n");
             Console.WriteLine("---------------------------------------------\n");
             return result;
         }
@@ -19,22 +19,21 @@ namespace Polymorphism
 
     public class Contractor : Employee
     {
-        public override void CalculateWeeklySalary(int weeklyHours, int wage)
+        public override string CalculateWeeklySalary(int weeklyHours, int wage)
         {
             var salary = weeklyHours * wage;
-            string expectedResponse =string.Format("\nThis HAPPY CONTRACTOR worked {0} hrs. " + 
-                              "Paid for {0} hrs at $ {1}" +
-                              "/hr = ${2} ", weeklyHours, wage, salary);
-            Console.WriteLine("\n" + expectedResponse + "\n");
-            Console.WriteLine("---------------------------------------------\n");
-            return expectedResponse;
+            string result = String.Format("This HAPPY CONTRACTOR worked {0} hrs. " +
+                            "Paid for {0} hrs at $ {1}" +
+                            "/hr = ${2} ", weeklyHours, wage, salary);
+            Console.WriteLine("\n" + result + " \n");
+            return result;
         }
     }
 
 
+
     public class Program
     {
-
         private static void Main(string[] args)
         {
             const int hours = 55, wage = 70;
